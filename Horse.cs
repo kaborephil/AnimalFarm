f800c6e610bf13b0e06483ddea17206604317772
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-03-29 17:43:14 -0400

    Add horse class and override base class method SPeak()

---
+++ Horse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    public class Horse : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Hello, my name is " + AnimalName +" and Iam a horse. I say neigh.");
+        }
+    }
+}
