4f867fd754b6e38df3cd266e2da3df7d62882dc7
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-03-29 17:30:43 -0400

    Add abstract base class

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    public abstract class Animal
+    {
+        public string AnimalName { get; set; }
+
+        public abstract void Speak();
+        
+    }
+}
