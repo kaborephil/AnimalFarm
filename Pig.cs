06028ac0c6c07fbeefc25ce10fd051a198849b6a
Pingdwende Philippe Kabore <kaborephil@gmail.com>
2019-03-29 17:54:33 -0400

    Add pig class and override Speak()

---
+++ Pig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    public class Pig : Animal
+    {
+        //Polimorphism
+        public override void Speak()
+        {
+            Console.WriteLine("Hello, my name is " + AnimalName + " and I am a pig. I say oink.");
+        }
+    }
+}
